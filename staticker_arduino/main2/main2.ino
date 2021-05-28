047ecb5d1f7b285d613944499a1515025357f013
leuchtum <acct.d-mueller@mailbox.org>
2021-05-28 14:34:21 +0200

    Implemeted one running button on arduino (with Bounce2)

--- staticker_arduino/main2/main2.ino
+++ staticker_arduino/main2/main2.ino
@@ -1,4 +1,5 @@
 #include <ArduinoJson.h>
+#include <Bounce2.h>
 
 
 // Serial input
@@ -6,22 +7,49 @@ String serial_input_string = "";
 bool serial_input_string_complete = false;
 
 
+// Button General
+const int min_pressed_time = 300;
+
+
+// Button GoalWhiteDefense
+const int GWD_PIN = 6;
+Bounce gwd_button = Bounce();
+
+
 void setup() {
   Serial.begin(9600);
   // reserve 200 bytes for the serial_input_string:
   serial_input_string.reserve(200);
+
+  // Init bounce objects
+  gwd_button.attach (GWD_PIN,INPUT_PULLUP);
+  gwd_button.interval(25);
 }
 
 
 void loop() {
+  // Check for incoming commands
   check_serial_input();
+  
+  // Update bounce objects
+  gwd_button.update();
 
+  // Check if state of button changed and if so process change
+  check_button(gwd_button, "gwd");
+
+}
+
+
+void check_button(Bounce button, String button_indent){
+  if (button.fell()){
+    send("pressed", button_indent);
+  }
 }
 
 
 void decode_serial_input(){
   // Init JSON document
-  StaticJsonDocument<200> json_dict;
+  StaticJsonDocument<128> json_dict;
 
   // Deserialize the JSON document
   DeserializationError err = deserializeJson(json_dict, serial_input_string);
@@ -37,10 +65,10 @@ void decode_serial_input(){
 
   // Process mode setLED
   if(mode == "setLED"){
-    const char* led_bd = json_dict["bd"];
-    const char* led_bo = json_dict["bo"];
-    const char* led_wd = json_dict["wd"];
-    const char* led_wo = json_dict["wo"];
+    const char* led_bd = json_dict["lbd"];
+    const char* led_bo = json_dict["lbo"];
+    const char* led_wd = json_dict["lwd"];
+    const char* led_wo = json_dict["lwo"];
     bool all_exist = led_bd && led_bo && led_wd && led_wo;
     raise_error_if_missing_key(all_exist);
     if (all_exist){
@@ -55,6 +83,11 @@ void decode_serial_input(){
 }
 
 
+void send(String mode, String msg){
+  core_send(mode, msg);
+}
+
+
 void raise_error_if_missing_key(bool all_ok){
   if (!all_ok){
     raise_error("MissingKey");
@@ -63,10 +96,15 @@ void raise_error_if_missing_key(bool all_ok){
 
 
 void raise_error(String err){
-  StaticJsonDocument<200> json_error;
-  json_error["type"] = "error";
-  json_error["msg"] = err;
-  serializeJson(json_error, Serial);
+  core_send("error",err);
+}
+
+
+void core_send(String mode, String msg){
+  StaticJsonDocument<64> json;
+  json["mode"] = mode;
+  json["msg"] = msg;
+  serializeJson(json, Serial);
   Serial.print("\n");
 }
 
