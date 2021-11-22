f80ad5ac2522baad8790241735adb597cd851313
Daniel Müller <acct.d-mueller@mailbox.org>
2021-11-22 23:07:15 +0100

    Switch to ArduinoMega

--- staticker_arduino/main/main.ino
+++ staticker_arduino/main/main.ino
@@ -10,83 +10,85 @@ bool serial_input_string_complete = false;
 const int ARRAYLEN = 10;
 
 // LED WhiteDefense
-const int WD_LED_PIN = 4;
+const int WD_LED_PIN = 26;
 Adafruit_NeoPixel wd_led(ARRAYLEN, WD_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // LED WhiteOffense
-const int WO_LED_PIN = 7;
+const int WO_LED_PIN = 34;
 Adafruit_NeoPixel wo_led(ARRAYLEN, WO_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // LED WhiteScore
-const int WS_LED_PIN = 19;
+const int WS_LED_PIN = 8;
 Adafruit_NeoPixel ws_led(ARRAYLEN, WS_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // LED BlackDefense
-const int BD_LED_PIN = 10;
+const int BD_LED_PIN = 42;
 Adafruit_NeoPixel bd_led(ARRAYLEN, BD_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // LED BlackOffense
-const int BO_LED_PIN = 13;
+const int BO_LED_PIN = 50;
 Adafruit_NeoPixel bo_led(ARRAYLEN, BO_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // LED BlackScore
-const int BS_LED_PIN = 18;
+const int BS_LED_PIN = 9;
 Adafruit_NeoPixel bs_led(ARRAYLEN, BS_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Button General
-const int DEBOUNCETIME = 30;
-const int UNDOTIME = 1500;
+const int DEBOUNCETIME = 50;
+const int UNDOTIME = 1000;
 
 // Button GoalWhiteDefense
-const int GWD_BUTTON_PIN = 3;
+const int GWD_BUTTON_PIN = 24;
 Bounce2::Button gwd_button = Bounce2::Button();
 int gwd_press_started = 0;
 bool gwd_sended = true;
 
 // Button OwnerWhiteDefense
-const int OWD_BUTTON_PIN = 2;
+const int OWD_BUTTON_PIN = 22;
 Bounce2::Button owd_button = Bounce2::Button();
 int owd_press_started = 0;
 bool owd_sended = true;
 
 // Button GoalWhiteOffense
-const int GWO_BUTTON_PIN = 6;
+const int GWO_BUTTON_PIN = 32;
 Bounce2::Button gwo_button = Bounce2::Button();
 int gwo_press_started = 0;
 bool gwo_sended = true;
 
 // Button OwnerWhiteOffense
-const int OWO_BUTTON_PIN = 5;
+const int OWO_BUTTON_PIN = 30;
 Bounce2::Button owo_button = Bounce2::Button();
 int owo_press_started = 0;
 bool owo_sended = true;
 
 // Button GoalBlackDefense
-const int GBD_BUTTON_PIN = 9;
+const int GBD_BUTTON_PIN = 40;
 Bounce2::Button gbd_button = Bounce2::Button();
 int gbd_press_started = 0;
 bool gbd_sended = true;
 
 // Button OwnerBlackDefense
-const int OBD_BUTTON_PIN = 8;
+const int OBD_BUTTON_PIN = 38;
 Bounce2::Button obd_button = Bounce2::Button();
 int obd_press_started = 0;
 bool obd_sended = true;
 
 // Button GoalBlackOffense
-const int GBO_BUTTON_PIN = 12;
+const int GBO_BUTTON_PIN = 48;
 Bounce2::Button gbo_button = Bounce2::Button();
 int gbo_press_started = 0;
 bool gbo_sended = true;
 
 // Button OwnerBlackOffense
-const int OBO_BUTTON_PIN = 11;
+const int OBO_BUTTON_PIN = 46;
 Bounce2::Button obo_button = Bounce2::Button();
 int obo_press_started = 0;
 bool obo_sended = true;
 
 void setup() {
-  Serial.begin(9600);
+  // Begin serial
+  Serial.begin(115200);
+  
   // reserve 200 bytes for the serial_input_string:
   serial_input_string.reserve(200);
 
@@ -115,7 +117,6 @@ void setup() {
   bs_led.clear();
   bs_led.show();
   
-  
   // Init button objects
   gwd_button.attach (GWD_BUTTON_PIN,INPUT_PULLUP);
   gwd_button.interval(DEBOUNCETIME);
@@ -138,16 +139,13 @@ void setup() {
   obd_button.interval(DEBOUNCETIME);
   obd_button.setPressedState(LOW);
 
-  
   gbo_button.attach (GBO_BUTTON_PIN,INPUT_PULLUP);
   gbo_button.interval(DEBOUNCETIME);
   gbo_button.setPressedState(LOW);
   obo_button.attach (OBO_BUTTON_PIN,INPUT_PULLUP);
   obo_button.interval(DEBOUNCETIME);
   obo_button.setPressedState(LOW);
-  
 }
-
 
 void loop() {
   // Check for incoming commands
@@ -165,9 +163,7 @@ void loop() {
   
   check_button(gbo_button, gbo_press_started, gbo_sended, "gbo");
   check_button(obo_button, obo_press_started, obo_sended, "obo");
-  
 }
-
 
 void check_button(Bounce2::Button & button, int & press_started, bool & sended, String button_indent){
   // Init variables
@@ -199,7 +195,6 @@ void check_button(Bounce2::Button & button, int & press_started, bool & sended, 
     sended = true;
   }
 }
-
 
 void decode_serial_input(){
   // Init JSON document
@@ -313,7 +308,7 @@ void decode_serial_input(){
       send("echo","clear bs");
     }
     
-    else if (msg == "all"){
+    else if (msg == "all"){ // {"mode":"clear","msg": "all"}
       wd_led.clear();
       wd_led.show();
 
@@ -344,7 +339,6 @@ void decode_serial_input(){
   }
 }
 
-
 void set_led(Adafruit_NeoPixel & led, int red[ARRAYLEN], int green[ARRAYLEN], int blue[ARRAYLEN]){
   for (int i=0; i<ARRAYLEN; i++){
     led.setPixelColor(i, led.Color(red[i], green[i], blue[i]));
@@ -352,16 +346,13 @@ void set_led(Adafruit_NeoPixel & led, int red[ARRAYLEN], int green[ARRAYLEN], in
   led.show();
 }
 
-
 void send(String mode, String msg){
   core_send(mode, msg);
 }
 
-
 void raise_error(String err){
   core_send("error",err);
 }
-
 
 void core_send(String mode, String msg){
   StaticJsonDocument<64> json;
@@ -373,7 +364,6 @@ void core_send(String mode, String msg){
   Serial.print("\n");
 }
 
-
 void check_serial_input(){
   if (serial_input_string_complete) {
     // Execute encoded command
@@ -384,7 +374,6 @@ void check_serial_input(){
     serial_input_string_complete = false;
   }
 }
-
 
 void serialEvent() {
   // This function is called automatically after each loop
