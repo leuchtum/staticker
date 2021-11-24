d3a10845045251bbaf9624d2679b43397ad8cf9c
Daniel Müller <acct.d-mueller@mailbox.org>
2021-11-24 18:07:30 +0100

    Char violation in player new names

--- staticker/routes/player.py
+++ staticker/routes/player.py
@@ -8,10 +8,7 @@ from ..dependencies import templates, manager
 from ..core import NotFoundError, get_player_by_id, Player
 
 
-router = APIRouter(
-    prefix="/player",
-    tags=["player"]
-)
+router = APIRouter(prefix="/player", tags=["player"])
 
 
 @router.get("/", response_class=HTMLResponse)
@@ -29,24 +26,28 @@ async def player(request: Request, player_id: str, created: bool = False):
         p = get_player_by_id(player_id)
     except NotFoundError:
         raise HTTPException(status_code=404, detail="Player not found")
-    dic = {"request": request,
-           "player": p,
-           "created": created}
+    dic = {"request": request, "player": p, "created": created}
     return templates.TemplateResponse("player.html", dic)
-    
-    
+
+
 @router.get("/new", response_class=HTMLResponse)
-async def new_player(request: Request, exists: bool = False):
-    dic = {"request": request, "exists": exists}
+async def new_player(request: Request, exists: bool = False, errchars: bool = False):
+    dic = {"request": request, "exists": exists, "errchars": errchars}
     return templates.TemplateResponse("new_player.html", dic)
 
 
 @router.post("/new/submit")
 async def new_player_submit(_: Request, name: str = Form(...)):
-    p = Player(name=name)
+    allowed_chars = set(
+        "0123456789abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ_-"
+    )
     code = status.HTTP_302_FOUND
+    if set(name).issubset(allowed_chars):
+        p = Player(name=name)
+    else:
+        return RedirectResponse(url="/player/new?errchars=1", status_code=code)
     try:
         p.save()
     except peewee.IntegrityError:
-        return RedirectResponse(url='/player/new?exists=1', status_code=code)
+        return RedirectResponse(url="/player/new?exists=1", status_code=code)
     return RedirectResponse(url=f"/player/id/{p.id}?created=1", status_code=code)
