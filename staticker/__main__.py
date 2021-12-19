1e2bb19197aa7ae5b737e6db67c1bbcb6876a89a
Daniel Müller <acct.d-mueller@mailbox.org>
2021-12-19 13:30:34 +0100

    add debug options in main

--- staticker/__main__.py
+++ staticker/__main__.py
@@ -2,6 +2,15 @@ from .log import logger  # Must be imported before .app!
 from .app import app
 import uvicorn
 
+DEBUG = True
+
+if DEBUG:
+    HOST = "127.0.0.1"
+    PORT = 8000
+else:
+    HOST = "0.0.0.0"
+    PORT = 80
+
 if __name__ == "__main__":
     logger.info("Starting staticker")
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host=HOST, port=PORT)
